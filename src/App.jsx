ef7b9df51fab2c9ad3bca8087b61fe7c4ca166d4
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-07-25 23:46:24 +0300

    feat: set board title according to store in Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 export default function App() {
   return (
     <>
-      <Header appTitle="Drag & Drop Taskboard" boardTitle="Taskboard" />
+      <Header appTitle="Drag & Drop Taskboard" />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,22 @@
-export default function Header({ appTitle, boardTitle }) {
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setActiveBoardId } from "../store/slices/boardSlice";
+
+export default function Header({ appTitle }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // It is assumed that there is at least 1 board in the database. So the activeBoardId is set to "0" directly.
+    dispatch(setActiveBoardId("0"));
+  }, [dispatch]);
+
+  const activeBoardId = useSelector((state) => state.activeBoardId);
+  const boardTitle = useSelector(
+    (state) =>
+      state.boards.find((item) => item.id === activeBoardId)?.title ??
+      "Untitled"
+  );
+
   return (
     <header className="bg-orange-400 px-4 py-2">
       <h1 className="py-2 text-3xl font-semibold">{appTitle}</h1>
