f57e4335c34ec7f7abb43e57528320dee7fbaa21
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-07-26 16:08:10 +0300

    feat: add "edit" buton into task item

--- src/components/Board/TaskListItem.jsx
+++ src/components/Board/TaskListItem.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
 import Button from "../../shared/Button";
 
 export default function TaskListItem({ itemData }) {
@@ -8,12 +8,17 @@ export default function TaskListItem({ itemData }) {
   return (
     <div
       key={taskId}
-      className="flex justify-between items-center py-3 px-4 rounded-md bg-orange-50 cursor-pointer border border-orange-300"
+      className="flex justify-between items-center py-3 px-4 rounded-md bg-orange-50 cursor-pointer border border-orange-300 group"
     >
-      {description}
-      <Button title="Delete Task" iconBtn bgTransparent>
-        <FontAwesomeIcon icon={faTrash} />
-      </Button>
+      <p>{description}</p>
+      <div className="flex justify-end items-center gap-2 opacity-20 group-hover:opacity-100 transition-opacity">
+        <Button className="px-1.5" title="Edit Task" iconBtn bgTransparent>
+          <FontAwesomeIcon icon={faPen} />
+        </Button>
+        <Button className="px-1.5" title="Delete Task" iconBtn bgTransparent>
+          <FontAwesomeIcon icon={faTrash} />
+        </Button>
+      </div>
     </div>
   );
 }
